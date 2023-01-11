687d8ab7d3b3a8e0a0d718e42c2e48c4ba401923
asmaaelhadad <asmaaelhadad@gmail.com>
2023-01-11 13:02:39 +0100

    first and second iterations

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 // Iteration 1: Names and Input
+let hacker1= 'Asmaa' ;
+console.log(`The driver's name is ${hacker1}`);
 
+let hacker2= 'Asmaa' ;
+console.log(`The Navigator's name is ${hacker2}`);
 
 // Iteration 2: Conditionals
 
+let x= hacker1.length;
+let y=hacker2.length ;
+if (x>y){console.log(`The driver has the longest name, it has ${x} 
+ characters`) ;}
+else if (y>x){console.log(`It seems that the navigator has the longest name, it has ${y} characters.`) ;}
+
+else if (y=x){console.log(`Wow, you both have equally long names,  ${y} characters!`) ;}
+
+else {console.log('undefined');}
 
 // Iteration 3: Loops
